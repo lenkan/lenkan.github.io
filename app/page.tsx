f2aa6dca1fb90b580b58d088d1e292041ca671f6
lenkan <dlenksjo@gmail.com>
2024-04-01 12:02:16 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Page } from "./components/Page.tsx";
+import { Logo } from "./components/Logo.tsx";
+
+export default function Index() {
+  return (
+    <Page>
+      <Logo>lenkan.dev</Logo>
+    </Page>
+  );
+}
